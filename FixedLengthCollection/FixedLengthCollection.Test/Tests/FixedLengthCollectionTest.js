dd59a961f785ac6b27276aa69e490d1f3b206595
chad ruppert <chad.ruppert@gmail.com>
2013-12-20 09:24:51 -0500

    first checkin, has jasmine tests and phantom js. runs awesome in the resharper testrunner

---
+++ FixedLengthCollection/FixedLengthCollection.Test/Tests/FixedLengthCollectionTest.js
@@ -0,0 +1,122 @@
+﻿///<reference path="/../FixedLengthCollection/FixedLengthCollection.js"/>
+///<reference path="/lib/jasmine.js"/>
+
+
+describe("FixedLengthCollection push Tests", function() {
+
+    var coll = undefined;
+
+    beforeEach(function() {
+        coll = new fixedLengthCollection();
+        
+    });
+
+    it("should allow construction with an existing array", function() {
+
+        coll = new fixedLengthCollection(null, [1, 2, 3]);
+
+
+        expect(coll.length).toBe(3);
+        expect(coll[0]).toBe(1);
+        expect(coll[2]).toBe(3);
+    });
+
+    it("should not exceed length when pushing an item when duplicationcheck is not defined", function() {
+
+        coll = new fixedLengthCollection({ maxLength: 2 });
+
+        coll.push(1);
+        coll.push(2);
+        coll.push(3);
+
+        expect(coll.length).toBe(2);
+
+    });
+    
+    it("should not add duplicate item when pushing an item when dupecheck is on", function () {
+
+        coll = new fixedLengthCollection({
+            maxLength: 2,
+            enableDupeCheck: true
+        });
+
+        coll.push(1);
+        coll.push(1);
+        
+
+        expect(coll.length).toBe(1);
+
+    });
+    
+    it("should add duplicate item when pushing an item when dupecheck is off", function () {
+
+        coll = new fixedLengthCollection({
+            maxLength: 2,
+            enableDupeCheck: false
+        });
+
+        coll.push(1);
+        coll.push(1);
+
+
+        expect(coll.length).toBe(2);
+
+    });
+
+    it("should use equality test when pushing and test is supplied and dupeCheck is on", function() {
+        var opts = {
+            maxLength: 3,
+            enableDupeCheck: true,
+            equalityTest: function (collItem, item) {
+                return collItem.randomProperty == item.randomProperty;
+            }
+        };
+        coll = new fixedLengthCollection(opts, [{ randomProperty: 1 }, { randomProperty: 2 }]);
+
+        spyOn(opts, 'equalityTest').andCallThrough();
+
+        coll.push({ randomProperty: 2 });
+
+        expect(opts.equalityTest).toHaveBeenCalled();
+        expect(coll.length).toBe(2);
+    });
+    
+    it("should not use equality test when pushing and test is supplied and dupeCheck is off", function () {
+        var opts = {
+            maxLength: 3,
+            enableDupeCheck: false,
+            equalityTest: function (collItem, item) {
+                return collItem.randomProperty == item.randomProperty;
+            }
+        };
+        coll = new fixedLengthCollection(opts, [{ randomProperty: 1 }, { randomProperty: 2 }]);
+
+        spyOn(opts, 'equalityTest').andCallThrough();
+
+        coll.push({ randomProperty: 2 });
+
+        expect(opts.equalityTest).not.toHaveBeenCalled();
+        expect(coll.length).toBe(3);
+    });
+
+    it("should not add duplicate items from a splice(idx, countRemove, items...itemsX) call", function() {
+        coll = new fixedLengthCollection({
+            maxLength: 10,
+            enableDupeCheck: true
+        },[1,2,3,4,5]);
+
+        coll.splice(0, 0, 4, 5, 6);
+
+        expect(coll.length).toBe(6);
+    });
+    it("should  add duplicate items from a splice(idx, countRemove, items...itemsX) call", function () {
+        coll = new fixedLengthCollection({
+            maxLength: 10,
+            enableDupeCheck: false
+        }, [1, 2, 3, 4, 5]);
+
+        coll.splice(0, 0, 4, 5, 6);
+
+        expect(coll.length).toBe(8);
+    });
+});
---
+++ FixedLengthCollection/FixedLengthCollection/FixedLengthCollection.js
@@ -0,0 +1,83 @@
+﻿// concept and structure from here : http://www.bennadel.com/blog/2308-Creating-A-Fixed-Length-Queue-In-JavaScript-Using-Arrays.htm
+function fixedLengthCollection(opts, array) {
+    opts = opts ||  {maxLength:10, enableDupeCheck:false};
+
+    array = array || [];
+    var arr = Array.apply(null, array);
+    arr.opts = opts;
+    arr.push = fixedLengthCollection.push;
+    arr.unshift = fixedLengthCollection.unshift;
+    arr.splice = fixedLengthCollection.splice;
+    
+    return arr;
+};
+
+fixedLengthCollection.trimHead = function() {
+    if (this.length <= this.opts.maxLength) {
+        return;
+    }
+    Array.prototype.splice.call(this, 0, this.length - this.opts.maxLength);
+};
+fixedLengthCollection.trimTail = function() {
+    if (this.length <= this.opts.maxLength) {
+        return;
+    }
+    Array.prototype.splice.call(this, thix.opts.maxLength, this.length - this.opts.maxLength);
+};
+
+fixedLengthCollection.wrapMethod = function(methodName, trimAction) {
+    
+    
+    var wrapper = function () {
+
+        var isSplice = arguments.length > 2;
+        
+        if (this.opts.enableDupeCheck) {
+            if (!isSplice) {
+                if (this.opts.equalityTest !== undefined) {
+                    for (var i = 0; i < this.length; i++) {
+                        if (this.opts.equalityTest(this[i], arguments[0])) {
+                            return false;
+                        }
+                    }
+                } else {
+                    for (var i = 0; i < this.length; i++) {
+                        if (this[i] == arguments[0]) {
+                            return false;
+                        }
+                    }
+                }
+            }
+            else {
+                var itemsBeingAdded = Array.prototype.slice.call(arguments, 2);
+                var originalItemsLength = itemsBeingAdded.length;
+                var workingSet = itemsBeingAdded.slice(0);
+                if (this.opts.equalityTest !== undefined) {
+                    for (var i = 0; i < this.length; i++) {
+                        if (this[i] == arguments[0]) {
+                            return false;
+                        }
+                    }
+                } else {
+                    for (var i = 0; i < itemsBeingAdded.length; i++) {
+                        var idx = workingSet.indexOf(itemsBeingAdded[i]) > 0;
+                        if (idx > 0) {
+                            workingSet.splice(idx, 1);
+                        }
+                    }
+                }
+                arguments = Array.prototype.splice.call(arguments,2, originalItemsLength, workingSet);
+            }
+        } 
+        var method = Array.prototype[methodName];
+        var res = method.apply(this, arguments);
+        trimAction.call(this);
+        console.log(this.length);
+        return res;
+    };
+    return wrapper;
+};
+
+fixedLengthCollection.push = fixedLengthCollection.wrapMethod("push", fixedLengthCollection.trimHead);
+fixedLengthCollection.splice = fixedLengthCollection.wrapMethod("splice", fixedLengthCollection.trimTail);
+fixedLengthCollection.unshift = fixedLengthCollection.wrapMethod("unshift", fixedLengthCollection.trimTail);
